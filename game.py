7ae822c9dd6aea57936080b42ddd8e0578d55016
maqaro <basilnasr@outlook.com>
2024-08-28 19:33:01 +0100

    obstacle moving, spawning and despawn. Game Over Screen, obstacle collisions. Removed jump spamming.

--- game.py
+++ game.py
@@ -19,26 +19,30 @@ class Robot(pygame.sprite.Sprite):
         self.jumping = False
         self.jump_height = 0
 
+        self.score = 0
+
     def jump(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE]:
-            self.jumping = True
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 720:
+            self.jumping = True 
 
     def gravity(self):
         if self.jumping:
-            self.rect.y -= 10
-            self.jump_height += 10
+            self.rect.y -= 8
+            self.jump_height += 8
 
-        if self.jump_height >= 100:
+        if self.jump_height >= 180:
             self.jumping = False
             self.jump_height = 0
 
         if not self.jumping:
-            self.rect.y += 10 
+            self.rect.y += 8 
 
         if self.rect.bottom >= 720:
             self.rect.bottom = 720
 
+    def increase_score(self):
+        self.score += 1
 
     def update(self):
         self.jump()
@@ -49,7 +53,7 @@ class obstacle(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
-        self.image = pygame.image.load("assets/obstacle-1.png").convert_alpha()
+        self.image = pygame.image.load("assets/obstacle-1.png")
 
         original_width, original_height = self.image.get_size()
         new_width = int(original_width * 2.5)
@@ -57,17 +61,23 @@ class obstacle(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (new_width, new_height))
 
         self.rect = self.image.get_rect()
-        self.rect.center = (360, 360)
+        self.rect.center = (1000, 680)
 
+        self.speed = 5
 
     def move(self):
-        pass
+        self.rect.x -= self.speed
+
+    def boundries(self):
+        if self.rect.right < 0:
+            self.despawn()
 
     def despawn(self):
         self.kill()
 
     def update(self):
-        pass
+        self.move()
+        self.boundries()
 
 class game:
     def __init__(self):
@@ -77,7 +87,7 @@ class game:
         self.running = True
         self.alive = True
         self.player_group = pygame.sprite.GroupSingle(Robot())
-        self.obstacles = pygame.sprite.Group()
+        self.obstacles = pygame.sprite.Group(obstacle())
         self.score = 0
 
         pygame.display.set_icon(pygame.image.load("assets/character-1.png"))
@@ -91,13 +101,12 @@ class game:
             self.draw()
 
     def handle_collision(self):
-        # if self.player_group and pygame.sprite.spritecollide(self.player_group.sprite, self.obstacles, False):
-        #     self.player_group.empty()
-        #     self.obstacles.empty()
-        #     return False
-        # else:
-        #     return True
-        return True
+        if self.player_group and pygame.sprite.spritecollide(self.player_group.sprite, self.obstacles, False):
+            self.player_group.empty()
+            self.obstacles.empty()
+            return False
+        else:
+            return True
 
     def handle_events(self):
         for event in pygame.event.get():
@@ -105,7 +114,7 @@ class game:
                 self.running = False
 
             if not self.alive and event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_S:
+                if event.key == pygame.K_s:
                     self.alive = True
                     self.player_group.add(Robot())
 
@@ -127,6 +136,19 @@ class game:
         if self.alive:
             self.player_group.draw(self.screen)
             self.obstacles.draw(self.screen)
+            pygame.display.flip()
+
+        else:
+            font = pygame.font.Font(None, 74)
+            text = font.render("You Died", True, (0, 0, 0))
+            text_rect = text.get_rect(center=(480, 360))
+            self.screen.blit(text, text_rect)
+
+            font = pygame.font.Font(None, 74)
+            text = font.render("Press S to start", True, (0, 0, 0))
+            text_rect = text.get_rect(center=(480, 420))
+            self.screen.blit(text, text_rect)
+
             pygame.display.flip()
 
 game = game()
